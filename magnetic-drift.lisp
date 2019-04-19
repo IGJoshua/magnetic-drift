67a96ce63b78c960243619f06d341626cfdab5b2
Joshua Suskalo <joshua@suskalo.org>
2019-04-19 08:34:49 -0500

    Draw a fullscreen car

--- magnetic-drift.lisp
+++ magnetic-drift.lisp
@@ -5,10 +5,19 @@
 (defvar *running* nil)
 
 (defvar *quad-stream* nil)
+(defvar *textures* nil)
 
-(defun-g fullscreen-quad-vert ((uvs :vec2))
-  (values (v! uvs 0 1)
-          uvs))
+(defun texture (filename)
+  (alexandria:if-let ((tex (gethash filename *textures*)))
+    tex
+    (setf (gethash filename *textures*) (dirt:load-image-to-texture filename))))
+
+(defun sampler-for-texture (filename)
+  (sample (texture filename)))
+
+(defun-g fullscreen-quad-vert ((positions :vec2))
+  (values (v! positions 0 1)
+          (/ (+ (v! 1 1) positions) 2)))
 
 (defun-g red-frag ((uvs :vec2))
   (v! 1 0 0 1))
@@ -17,11 +26,21 @@
   :vertex (fullscreen-quad-vert :vec2)
   :fragment (red-frag :vec2))
 
+(defun-g texture-frag ((uvs :vec2)
+                       &uniform
+                       (sam :sampler-2d))
+  (texture sam uvs))
+
+(defpipeline-g textured-fullscreen-quad ()
+  :vertex (fullscreen-quad-vert :vec2)
+  :fragment (texture-frag :vec2))
+
 (defun render ()
   (clear)
   (setf (cepl:viewport-resolution (current-viewport))
         (surface-resolution (current-surface (cepl-context))))
-  (map-g #'red-fullscreen-quad *quad-stream*)
+  (map-g #'textured-fullscreen-quad *quad-stream*
+         :sam (sampler-for-texture "./res/car_blue_1.png"))
   (swap))
 
 (defun update (dt)
@@ -34,7 +53,9 @@
                                   :key #'slynk::channel-thread))
   (setf *running* t)
   (unless *quad-stream*
-    (setf *quad-stream* (nineveh:get-quad-stream-v2))))
+    (setf *quad-stream* (nineveh:get-quad-stream-v2)))
+  (unless *textures*
+    (setf *textures* (make-hash-table :test 'equal))))
 
 (defun run-loop ()
   (init)
