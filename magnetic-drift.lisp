23833932585262f8576257f772f8f2c621c05cb6
Joshua Suskalo <joshua@suskalo.org>
2019-04-19 08:58:05 -0500

    Allow the car to be sized

--- magnetic-drift.lisp
+++ magnetic-drift.lisp
@@ -35,12 +35,54 @@
   :vertex (fullscreen-quad-vert :vec2)
   :fragment (texture-frag :vec2))
 
+(defun-g quad-vert ((position :vec2)
+                    &uniform
+                    (quad->model :mat4)
+                    (model->world :mat4)
+                    (world->view :mat4)
+                    (view->projection :mat4))
+  (let* ((uvs (/ (+ (v! 1 1) position) 2.0))
+         (pos (v! position 0.0 1.0))
+         (pos (* quad->model pos))
+         (pos (* model->world pos))
+         (pos (* world->view pos))
+         (pos (* view->projection pos)))
+    (values pos
+            uvs)))
+
+(defpipeline-g textured-object-quad ()
+  :vertex (quad-vert :vec2)
+  :fragment (texture-frag :vec2))
+
+(defun ortho-projection ()
+  (destructuring-bind (x y) (cepl.viewports:viewport-dimensions (current-viewport))
+    (rtg-math.projection:orthographic (float x) (float y) 0.1 1000.0)))
+
+(defun view-matrix (pos scale)
+  (m! (/ (x scale)) 0 0 (- (x pos))
+      0 (/ (y scale)) 0 (- (y pos))
+      0 0 1 0
+      0 0 0 1))
+
 (defun render ()
   (clear)
   (setf (cepl:viewport-resolution (current-viewport))
         (surface-resolution (current-surface (cepl-context))))
-  (map-g #'textured-fullscreen-quad *quad-stream*
-         :sam (sampler-for-texture "./res/car_blue_1.png"))
+  (let* ((tex (texture "./res/car_blue_1.png"))
+         (sam (sample tex)))
+    (destructuring-bind (x y) (texture-base-dimensions tex)
+        (map-g #'textured-object-quad *quad-stream*
+               :quad->model (m! x 0 0 0
+                                0 y 0 0
+                                0 0 1 0
+                                0 0 0 1)
+               :model->world (m! 1 0 0 0
+                                 0 1 0 0
+                                 0 0 1 0
+                                 0 0 0 1)
+               :world->view (view-matrix (v! 0 0) (v! 5 5))
+               :view->projection (ortho-projection)
+               :sam sam)))
   (swap))
 
 (defun update (dt)
